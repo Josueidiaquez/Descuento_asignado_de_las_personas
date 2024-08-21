fcd8a2752c9119fcf72ee8e24a3f561ca9a2942a
Josueidiaquez <josueidiaquez4@gmail.com>
2024-08-21 13:05:17 -0600

    actualizacion

--- Descuento_asignado_de_las_personas/Program.cs
+++ Descuento_asignado_de_las_personas/Program.cs
@@ -1,2 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace Descuento_asignado_de_las_personas
+{
+    class Producto
+    {
+        static void Main(string[] args)
+        {
+            // en esta parte se debe colocar los datos de la categoria
+            Console.WriteLine("*** CATEGORIA ***");
+            Console.WriteLine("ingrese la categoria");
+            int categoria = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("*** PRODUCTO ***");
+            Console.WriteLine("ingrese el producto");
+            int producto = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("*** PRECIO ***");
+            Console.WriteLine("Ingrese el precio del producto");
+            int precio = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("*** TAMAÑO DEL PRODUCTO ***");
+            Console.WriteLine("Ingrese el tamaño del producto");
+            Console.WriteLine("1. Normal");
+            Console.WriteLine("2. Medio");
+            Console.WriteLine("3. Grande");
+            int tamaño = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("*** PRECIO DEL TAMAÑO");
+            Console.WriteLine("Ingrese el precio segun tamaño");
+            Console.WriteLine("1. Tamaño Normal");
+            Console.WriteLine("2. 25% mas de normal");
+            Console.WriteLine("3. 30% mas del medio");
+            int preciodeltamaño = Convert.ToInt16(Console.ReadLine());
+
+            int total;
+            total = precio + tamaño * preciodeltamaño;
+            Console.WriteLine("Total del producto: ", "{total}");
+        }
+    }
+}
